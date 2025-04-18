8d22ee594e0411b620526a666905c0c70f975aee
realjesse <154276383+realjesse@users.noreply.github.com>
2025-04-17 17:09:04 -0700

    Fix sound file path

--- week01/assets/js/soundFunctionality.js
+++ week01/assets/js/soundFunctionality.js
@@ -1,5 +1,5 @@
 const firstButtonNode = document.querySelector(".button.big.wide.smooth-scroll-middle");
-const soundEffect = new Audio('/sounds/mouse-click-290204.mp3');
+const soundEffect = new Audio('assets/sounds/mouse-click-290204.mp3');
 
 firstButtonNode.addEventListener('click', () => {
     soundEffect.play();
