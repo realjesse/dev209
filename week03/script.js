c0d6d7a12e9586cdb4e2e4c30cda45362d797874
realjesse <154276383+realjesse@users.noreply.github.com>
2025-04-28 12:33:09 -0700

    Update to clone node instead of direct ref

--- week03/script.js
+++ week03/script.js
@@ -2,11 +2,11 @@ const gridContainerNode = document.querySelector(".grid_container");
 
 const gridElement = document.createElement("section");
 gridElement.classList.add("grid_element");
-gridElement.style.backgroundColor("black");
+gridElement.style.backgroundColor = "black";
 
 function createGridElements() {
     for (i=0;i<16;i++) {
-        gridContainerNode.appendChild(gridElement)
+        gridContainerNode.appendChild(gridElement.cloneNode(true))
     }
 }
 
