31f78edd3650cb86b395d6f3a68719226f98da81
realjesse <154276383+realjesse@users.noreply.github.com>
2025-04-28 12:41:22 -0700

    Update class names for game_container

--- week03/script.js
+++ week03/script.js
@@ -2,7 +2,9 @@ const gridContainerNode = document.querySelector(".grid_container");
 
 const gridElement = document.createElement("section");
 gridElement.classList.add("grid_element");
-gridElement.style.backgroundColor = "black";
+gridElement.style.backgroundColor = "rgb(56, 180, 202)";
+gridElement.style.width = "15px";
+gridElement.style.height = "25px";
 
 function createGridElements() {
     for (i=0;i<16;i++) {
