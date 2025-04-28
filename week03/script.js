e837cb3fa0d04fc1df0b5cb7ed430cb6f96237c0
realjesse <154276383+realjesse@users.noreply.github.com>
2025-04-28 14:29:14 -0700

    Create cardList to store card data

--- week03/script.js
+++ week03/script.js
@@ -1,13 +1,24 @@
+// Get and create elements
 const gridContainerNode = document.querySelector(".grid_container");
-
 const gridElement = document.createElement("section");
 gridElement.classList.add("grid_element");
-gridElement.style.backgroundColor = "rgb(56, 180, 202)";
-gridElement.style.width = "15px";
-gridElement.style.height = "25px";
+
+// Potential letters for cards
+const letterList = ["A", "B", "C", "D", "E", "F", "G", "H"];
+// List that will contain card information
+const cardList = [];
+let currentCardIndex = 0;
+
+// For each letter in letterList, push two elements onto cardList which contain
+// that letter 
+letterList.forEach(letter => {
+    cardList.push({value: letter});
+    cardList.push({value: letter});
+})
 
 function createGridElements() {
     for (i=0;i<16;i++) {
+        const gridElementClone = gridElement.cloneNode(true);
         gridContainerNode.appendChild(gridElement.cloneNode(true))
     }
 }
