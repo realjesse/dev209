0e2ecedb59bf4ce0760a6d6103187ab777ca9f7a
realjesse <154276383+realjesse@users.noreply.github.com>
2025-04-28 12:08:27 -0700

    Implement basic grid creation functionality

--- week03/script.js
+++ week03/script.js
@@ -1,2 +1,13 @@
 const gridContainerNode = document.querySelector(".grid_container");
 
+const gridElement = document.createElement("section");
+gridElement.classList.add("grid_element");
+gridElement.style.backgroundColor("black");
+
+function createGridElements() {
+    for (i=0;i<16;i++) {
+        gridContainerNode.appendChild(gridElement)
+    }
+}
+
+createGridElements();
