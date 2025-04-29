3672acdf36d7f433cd5ac94547ea4dbd16734a66
realjesse <154276383+realjesse@users.noreply.github.com>
2025-04-28 17:47:06 -0700

    Create shuffleCards() functionality
    Using Fisher-Yates shuffle method.

--- week03/script.js
+++ week03/script.js
@@ -111,4 +111,13 @@ function checkForWin() {
     return true;
 }
 
+function shuffleCards() {
+    for (let i = cardList.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [cardList[i], cardList[j]] = [cardList[j], cardList[i]];
+    }
+}
+
+shuffleCards();
+
 createGridElements();
