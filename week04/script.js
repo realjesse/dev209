6366b92ade2594c783d9cb2db650ac0f7e39c94a
realjesse <154276383+realjesse@users.noreply.github.com>
2025-05-10 17:04:58 -0700

    Create basic login, register functionality

--- week04/script.js
+++ week04/script.js
@@ -1,9 +1,53 @@
-function loginUser(event) {
+const API_URL = "http://localhost:3000"
+
+async function loginUser(event) {
     event.preventDefault();
-    console.log("hi")
+    
+    const username = document.querySelector("#login_username").value;
+    const password = document.querySelector("#login_password").value;
+
+    try {
+        const response = await fetch(`${API_URL}/login`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ username, password }),
+        });
+
+        if (response.status === 200) {
+            alert("Success, login");
+        } else {
+            alert("Unsuccessful :(")
+        }
+    }
+    catch(error) {
+        console.log(error);
+    }
 }
 
-function registerUser(event) {
+async function registerUser(event) {
     event.preventDefault();
-    console.log("success!")
+
+    const username = document.querySelector("#register_username").value;
+    const password = document.querySelector("#register_password").value;
+
+    try {
+        const response = await fetch(`${API_URL}/register`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ username, password }),
+        });
+
+        if (response.status === 201) {
+            alert("Success, login");
+        } else {
+            alert("Unsuccessful :(")
+        }
+    }
+    catch(error) {
+        console.log(error);
+    }
 }
