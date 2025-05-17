84a4b6a4b3bfbd546eedd17f2389fe9a83a7acce
realjesse <154276383+realjesse@users.noreply.github.com>
2025-05-17 13:42:27 -0700

    Call LoginForm component in app.jsx

--- week05/my-app/src/App.jsx
+++ week05/my-app/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import LoginForm from './components/LoginForm';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -321,20 +322,7 @@ function closeOverlay() {
   return (
     <>
       <section id="login_register_container" className="">
-          <h1>Login</h1>
-          <form onSubmit={loginUser} id="login_form" className="">
-              <section className="form_group">
-                  <label htmlFor="login_username">Username:</label>
-                  <input name="username" type="text" id="login_username" required />
-              </section>
-              <section className="form_group">
-                  <label htmlFor="login_password">Password:</label>
-                  <input name="password" type="password" id="login_password" required />
-              </section>
-              <section className="submit_button_container">
-                  <button type="submit">Login</button>
-              </section>
-          </form>
+          <LoginForm></LoginForm>
           <h1>Register</h1>
           <form onSubmit={registerUser} id="register_form" className="">
               <section className="form_group">
