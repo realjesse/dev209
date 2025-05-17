def465b68686be7a17b15165565857a9335cdadd
realjesse <154276383+realjesse@users.noreply.github.com>
2025-05-16 18:57:32 -0700

    Functionality to update username and pass

--- week05/my-app/src/components/LoginForm.jsx
+++ week05/my-app/src/components/LoginForm.jsx
@@ -1,34 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function LoginForm() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
         <section id="login_register_container" className="">
             <h1>Login</h1>
             <form onSubmit={loginUser} id="login_form" className="">
                 <section className="form_group">
                     <label htmlFor="login_username">Username:</label>
-                    <input name="username" type="text" id="login_username" required />
+                    <input 
+                        name="username" 
+                        type="text" 
+                        id="login_username" 
+                        onChange={e => setUsername(e.target.value)} 
+                        required 
+                    />
                 </section>
                 <section className="form_group">
                     <label htmlFor="login_password">Password:</label>
-                    <input name="password" type="password" id="login_password" required />
+                    <input 
+                        name="password" 
+                        type="password" 
+                        id="login_password"
+                        onChange={e => setPassword(e.target.value)} 
+                        required 
+                    />
                 </section>
                 <section className="submit_button_container">
                     <button type="submit">Login</button>
-                </section>
-            </form>
-            <h1>Register</h1>
-            <form onSubmit={registerUser} id="register_form" className="">
-                <section className="form_group">
-                    <label htmlFor="register_username">Username:</label>
-                    <input name="username" type="text" id="register_username" required />
-                </section>
-                <section className="form_group">
-                    <label htmlFor="register_password">Password:</label>
-                    <input name="password" type="password" id="register_password" required />
-                </section>
-                <section className="submit_button_container">
-                    <button type="submit">Register</button>
                 </section>
             </form>
         </section>
